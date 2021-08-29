b35aba28b33e8ed76fe33fa52185af942d60c879
Eugene Oliveros <eevoliveros@gmail.com>
2021-08-29 19:03:01 +0800

    feat(nvim): use nvim-comment
    Just want to try out nvim-comment instead of kommentary.nvim

--- nvim/.config/nvim/lua/config/comments.lua
+++ nvim/.config/nvim/lua/config/comments.lua
@@ -1,1 +1,2 @@
-require('kommentary.config').configure_language('default', { prefer_single_line_comments = true })
+-- require('kommentary.config').configure_language('default', { prefer_single_line_comments = true })
+require('nvim_comment').setup({comment_empty = false})
