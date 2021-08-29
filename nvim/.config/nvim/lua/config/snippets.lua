3a67472c32052b88e9a7e9ea763d2ba8de3e2b11
Eugene Oliveros <eevoliveros@gmail.com>
2021-08-29 17:05:12 +0800

    fix(nvim): config cleanup

--- nvim/.config/nvim/lua/config/snippets.lua
+++ nvim/.config/nvim/lua/config/snippets.lua
@@ -1,12 +1,11 @@
 local luasnip = require('luasnip')
 
-luasnip.config.set_config({
+luasnip.config.setup({
   history = true,
   -- Update more often, :h events for more info.
   updateevents = 'TextChanged,TextChangedI',
 })
 
--- require("luasnip/loaders/from_vscode").load()
 require('luasnip/loaders/from_vscode').lazy_load()
 
 vim.keymap.inoremap({
