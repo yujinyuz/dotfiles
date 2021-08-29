093d1e2f7f04277555c87f3c29d78420022d6b4b
Eugene Oliveros <eevoliveros@gmail.com>
2021-08-29 17:01:52 +0800

    feat(nvim): replace snap with telescope
    telescope has gone massive improvements over the past couple of days and
    seems like it's already on par with snap.
    
    snap also hasn't been updated in awhile

--- nvim/.config/nvim/lua/config/snap.lua
+++ nvim/.config/nvim/lua/config/snap.lua
@@ -1,5 +1,5 @@
 local snap = require('snap')
-local file = snap.config.file:with({ reverse = true, consumer = 'fzf' })
+-- local file = snap.config.file:with({ reverse = true, consumer = 'fzf' })
 local vimgrep = snap.config.vimgrep:with({
   reverse = true,
   consumer = 'fzf',
@@ -7,24 +7,24 @@ local vimgrep = snap.config.vimgrep:with({
 })
 
 snap.maps({
-  {
-    '<leader><leader>',
-    file({
-      try = {
-        -- snap.get('producer.git.file').args({ '--cached', '--others', '--exclude-standard', ' | uniq' }),
-        snap.get('producer.git.file').args({ '--cached', '--others', '--exclude-standard', '--deduplicate' }),
-        'ripgrep.file',
-      },
-      prompt = 'Files',
-    }),
-    command = { 'files' },
-  },
-  {
-    '<leader>F',
-    vimgrep({
-      -- https://github.com/camspiers/snap/pull/66#issuecomment-873678089
-      producer = snap.get('producer.ripgrep.vimgrep').line({ '--smart-case', '--hidden' }),
-      prompt = 'Live Grep',
-    }),
-  },
+  -- {
+  --   '<leader><leader>',
+  --   file({
+  --     try = {
+  --       -- snap.get('producer.git.file').args({ '--cached', '--others', '--exclude-standard', ' | uniq' }),
+  --       snap.get('producer.git.file').args({ '--cached', '--others', '--exclude-standard', '--deduplicate' }),
+  --       'ripgrep.file',
+  --     },
+  --     prompt = 'Files',
+  --   }),
+  --   command = { 'files' },
+  -- },
+  -- {
+  --   '<leader>F',
+  --   vimgrep({
+  --     -- https://github.com/camspiers/snap/pull/66#issuecomment-873678089
+  --     producer = snap.get('producer.ripgrep.vimgrep').line({ '--smart-case', '--hidden' }),
+  --     prompt = 'Live Grep',
+  --   }),
+  -- },
 })
