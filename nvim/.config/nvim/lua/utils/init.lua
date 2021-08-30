aa645ba3e6637e9135ac8d2009fa822de340b912
Eugene Oliveros <eevoliveros@gmail.com>
2021-08-30 19:20:06 +0800

    feat(nvim): add sudo_write method

--- nvim/.config/nvim/lua/utils/init.lua
+++ nvim/.config/nvim/lua/utils/init.lua
@@ -1,6 +1,8 @@
 -- @usage dump(vim.opt)
 _G.dump = function(...)
-  print(vim.inspect(...))
+  local objects = vim.tbl_map(vim.inspect, { ... })
+  print(unpack(objects))
+  return ...
 end
 
 -- Just a simple wrapper around :autocmd in vim
@@ -129,6 +131,52 @@ function M.lsp_config()
     ret[client.name] = { root_dir = client.config.root_dir, settings = client.config.settings }
   end
   dump(ret)
+end
+
+function M.execute(id)
+  local func = M.functions[id]
+  if not func then
+    error('Function doest not exist: ' .. id)
+  end
+  return func()
+end
+
+-- https://github.com/ibhagwan/nvim-lua/blob/main/lua/utils.lua#L280
+function M.sudo_exec(cmd, print_output)
+local password = vim.fn.inputsecret("Password: ")
+  if not password or #password == 0 then
+      M.warn("Invalid password, sudo aborted")
+      return false
+  end
+  local out = vim.fn.system(string.format("sudo -p '' -S %s", cmd), password)
+  if vim.v.shell_error ~= 0 then
+    print("\r\r")
+    M.error(out)
+    return false
+  end
+  -- if print_output then print("\r\n", out) end
+  return true
+end
+
+function M.sudo_write(tmpfile, filepath)
+  if not tmpfile then tmpfile = vim.fn.tempname() end
+  if not filepath then filepath = vim.fn.expand("%") end
+  if not filepath or #filepath == 0 then
+    M.error("E32: No file name")
+    return
+  end
+  -- `bs=1048576` is equivalent to `bs=1M` for GNU dd or `bs=1m` for BSD dd
+  -- Both `bs=1M` and `bs=1m` are non-POSIX
+  local cmd = string.format("dd if=%s of=%s bs=1048576",
+    vim.fn.shellescape(tmpfile),
+    vim.fn.shellescape(filepath))
+  -- no need to check error as this fails the entire function
+  vim.api.nvim_exec(string.format("write! %s", tmpfile), true)
+  if M.sudo_exec(cmd) then
+    vim.cmd("e!")
+    M.info(string.format('"%s" written', filepath))
+  end
+  vim.fn.delete(tmpfile)
 end
 
 return M
