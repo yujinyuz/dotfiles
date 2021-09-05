426a10ec4849bb8732fbbedc75de1b2d8b6d8a36
Eugene Oliveros <eevoliveros@gmail.com>
2021-09-05 13:41:05 +0800

    fix(nvim): nvim-cmp icon and incorrect <Cmd>
    Since we are formatting nvim-cmp's icons, we want to make sure that an
    icon exists before we concatenate icon and kind

--- nvim/.config/nvim/lua/config/cmp.lua
+++ nvim/.config/nvim/lua/config/cmp.lua
@@ -41,7 +41,7 @@ cmp.setup({
       if vim.fn.pumvisible() == 1 then
         vim.fn.feedkeys(utils.t('<C-p>'), 'n')
       elseif require('luasnip').jumpable(-1) then
-        vim.fn.feedkeys(utils.t('<Cm>lua require("luasnip").jump(-1)<CR>'), 'n')
+        vim.fn.feedkeys(utils.t('<Cmd>lua require("luasnip").jump(-1)<CR>'), 'n')
       else
         fallback()
       end
@@ -57,7 +57,12 @@ cmp.setup({
   },
   formatting = {
     format = function(entry, vim_item)
-      vim_item.kind = require('config.lsp.kind').icons[vim_item.kind] .. ' ' .. vim_item.kind
+      local icon = require('config.lsp.kind').icons[vim_item.kind]
+
+      if icon then
+        vim_item.kind = icon .. ' ' .. vim_item.kind
+      end
+
       vim_item.menu = ({
         nvim_lsp = '[LSP]',
         nvim_lua = '[Lua]',
