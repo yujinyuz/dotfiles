ac8ab2e1acce8391f32df944e9af8aeac0b2a0d0
Eugene Oliveros <eevoliveros@gmail.com>
2021-10-03 17:18:46 +0800

    feat(nvim): add new ftplugin files
    I've added a bunch of ftplugin mainly for json, lua, python and markdown
    
    I've opted out on using `vim.opt_local` in favor of `vim.cmd
    [[setlocal]]` because of a bug where `wo` options are applied to the
    whole window instead of the local buffer inside a window

--- nvim/.config/nvim/after/ftplugin/lua.lua
+++ nvim/.config/nvim/after/ftplugin/lua.lua
@@ -1,2 +1,11 @@
 -- nvim runtime overrides our `formatoptions` just remove the unwated stuff here
 vim.opt_local.formatoptions:remove('o')
+
+require('cmp').setup.buffer({
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'nvim_lua' },
+    { name = 'tags' },
+    { name = 'path' },
+  },
+})
--- nvim/.config/nvim/after/ftplugin/markdown.lua
+++ nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -1,3 +1,14 @@
-vim.opt_local.spell = true
-vim.opt_local.conceallevel = 3
-vim.opt_local.concealcursor = 'n'
+-- vim.opt_local.spell = true
+-- vim.opt_local.conceallevel = 3
+-- vim.opt_local.concealcursor = 'n'
+
+vim.cmd [[
+  setlocal spell conceallevel=3 concealcursor=n
+]]
+
+require('cmp').setup.buffer({
+  sources = {
+    { name = 'buffer' },
+    { name = 'calc' },
+  },
+})
