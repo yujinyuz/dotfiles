b194eab39c8c6fb5a1399f42c5f1cbac584a8e51
Eugene Oliveros <eevoliveros@gmail.com>
2021-10-03 17:23:48 +0800

    feat(nvim): use snap for live_grep
    Also added find_files command just in case. This is actually
    undocumented but the inspiration for this can be found here:
    
    https://github.com/LunarVim/LunarVim/pull/582/files

--- nvim/.config/nvim/lua/config/snap.lua
+++ nvim/.config/nvim/lua/config/snap.lua
@@ -1,30 +1,32 @@
 local snap = require('snap')
--- local file = snap.config.file:with({ reverse = true, consumer = 'fzf' })
+local file = snap.config.file:with({ reverse = true, consumer = 'fzf' })
 local vimgrep = snap.config.vimgrep:with({
   reverse = true,
   consumer = 'fzf',
   limit = 50000,
 })
 
-snap.maps({
-  -- {
-  --   '<leader><leader>',
-  --   file({
-  --     try = {
-  --       -- snap.get('producer.git.file').args({ '--cached', '--others', '--exclude-standard', ' | uniq' }),
-  --       snap.get('producer.git.file').args({ '--cached', '--others', '--exclude-standard', '--deduplicate' }),
-  --       'ripgrep.file',
-  --     },
-  --     prompt = 'Files',
-  --   }),
-  --   command = { 'files' },
-  -- },
-  -- {
-  --   '<leader>F',
-  --   vimgrep({
-  --     -- https://github.com/camspiers/snap/pull/66#issuecomment-873678089
-  --     producer = snap.get('producer.ripgrep.vimgrep').line({ '--smart-case', '--hidden' }),
-  --     prompt = 'Live Grep',
-  --   }),
-  -- },
-})
+snap.register.command(
+  'find_files',
+  file({
+    try = {
+      -- snap.get('producer.git.file').args({ '--cached', '--others', '--exclude-standard', ' | uniq' }),
+      snap.get('producer.git.file').args({ '--cached', '--others', '--exclude-standard', '--deduplicate' }),
+      'ripgrep.file',
+    },
+    prompt = 'Files',
+  })
+)
+
+snap.register.command(
+  'live_grep',
+  vimgrep({
+    -- https://github.com/camspiers/snap/pull/66#issuecomment-873678089
+    producer = snap.get('producer.ripgrep.vimgrep').line({ '--smart-case', '--hidden', '--glob=!.git' }),
+    prompt = 'Live Grep',
+  })
+)
+
+vim.keymap.nnoremap({ '<leader>F', '<Cmd>Snap live_grep<CR>' })
+vim.cmd([[hi link SnapPosition Special]])
+vim.cmd([[hi link SnapBorder TelescopeBorder]])
