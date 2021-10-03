f9209ea7659042656baaedc3f1b044964504c179
Eugene Oliveros <eevoliveros@gmail.com>
2021-10-03 17:35:28 +0800

    chore(nvim): format null-ls config

--- nvim/.config/nvim/lua/config/lsp/null-ls.lua
+++ nvim/.config/nvim/lua/config/lsp/null-ls.lua
@@ -1,5 +1,4 @@
 local nls = require('null-ls')
-
 local M = {}
 
 local sources = {
@@ -8,10 +7,10 @@ local sources = {
   nls.builtins.formatting.black,
   nls.builtins.formatting.isort,
   nls.builtins.formatting.stylua,
-  nls.builtins.diagnostics.misspell.with({filetypes = {'markdown', 'txt'}}),
+  nls.builtins.diagnostics.misspell.with({ filetypes = { 'markdown', 'txt' } }),
   -- nls.builtins.diagnostics.write_good,
   nls.builtins.diagnostics.flake8,
-  nls.builtins.diagnostics.eslint.with({command = "eslint_d"}),
+  nls.builtins.diagnostics.eslint.with({ command = 'eslint_d' }),
   -- nls.builtins.diagnostics.eslint,
   nls.builtins.code_actions.gitsigns,
 }
@@ -25,7 +24,6 @@ function M.setup()
 end
 
 function M.has_formatter(ft)
-
   local formatters = require('null-ls.generators').get_available(ft, require('null-ls.methods').internal.FORMATTING)
   for _, f in ipairs(formatters) do
     if vim.tbl_contains(f.filetypes, ft) then
