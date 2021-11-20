9df2aedaf83d35d6220315a2be3aeae123125e2e
Eugene Oliveros <eevoliveros@gmail.com>
2021-11-20 14:15:32 +0800

    fix(nvim): update autopais plugin

--- nvim/.config/nvim/lua/config/autopairs.lua
+++ nvim/.config/nvim/lua/config/autopairs.lua
@@ -1,10 +1,4 @@
 require('nvim-autopairs').setup({
   disable_filetype = { 'TelescopePrompt', 'vim' },
-  close_triple_quotes = true,
-})
-
-require('nvim-autopairs.completion.cmp').setup({
-  map_cr = true,
-  map_complete = true,
-  auto_select = false,
+  map_c_w = true,
 })
