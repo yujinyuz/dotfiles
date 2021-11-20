0580f04780ed52df6096479bbf1883d9caa1f20a
Eugene Oliveros <eevoliveros@gmail.com>
2021-11-20 14:09:01 +0800

    feat(nvim): replace cmp source tags with rg
    I find ripgrep much more reliable completion compared to tags

--- nvim/.config/nvim/after/ftplugin/markdown.lua
+++ nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -2,13 +2,15 @@
 -- vim.opt_local.conceallevel = 3
 -- vim.opt_local.concealcursor = 'n'
 
-vim.cmd [[
+vim.cmd([[
   setlocal spell conceallevel=3 concealcursor=n
-]]
+]])
 
 require('cmp').setup.buffer({
   sources = {
-    { name = 'buffer' },
+    { name = 'look', keyword_length = 2, max_item_count = 10, opts = { convert_case = true, loud = true } },
+    { name = 'rg', max_item_count = 10, keyword_length = 3 },
+    { name = 'buffer', max_item_count = 10, keyword_length = 3 },
     { name = 'calc' },
   },
 })
