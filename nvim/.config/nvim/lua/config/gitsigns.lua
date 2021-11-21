0cbb272eb7528534195151bd2aafb349e793bbf3
Eugene Oliveros <eevoliveros@gmail.com>
2021-11-21 11:58:31 +0800

    feat(nvim): update gitsigns config and keymaps
    Looks pretty decent for me to have a diff in my code and a bunch of hunk
    previews for a better workflow
    
    I was also able to remove a dependency which gitsigns provides out of
    the box

--- nvim/.config/nvim/lua/config/gitsigns.lua
+++ nvim/.config/nvim/lua/config/gitsigns.lua
@@ -1,1 +1,29 @@
-require('gitsigns').setup({ signcolumn = false })
+require('gitsigns').setup({
+  signcolumn = false,
+  current_line_blame = false,
+  current_line_blame_opts = {
+    virt_text = true,
+    virt_text_pos = 'eol',
+    delay = 500,
+  },
+  keymaps = {
+
+    ['n ]c'] = { expr = true, "&diff ? ']c' : '<cmd>lua require\"gitsigns.actions\".next_hunk()<CR>'" },
+    ['n [c'] = { expr = true, "&diff ? '[c' : '<cmd>lua require\"gitsigns.actions\".prev_hunk()<CR>'" },
+
+    ['n <leader>ghs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
+    ['v <leader>ghs'] = '<cmd>lua require"gitsigns".stage_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
+    ['n <leader>ghu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
+    ['n <leader>ghr'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
+    ['v <leader>ghr'] = '<cmd>lua require"gitsigns".reset_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
+    ['n <leader>ghR'] = '<cmd>lua require"gitsigns".reset_buffer()<CR>',
+    ['n <leader>ghp'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
+    ['n <leader>ghb'] = '<cmd>lua require"gitsigns".blame_line{full=true}<CR>',
+    ['n <leader>ghS'] = '<cmd>lua require"gitsigns".stage_buffer()<CR>',
+    ['n <leader>ghU'] = '<cmd>lua require"gitsigns".reset_buffer_index()<CR>',
+
+    -- Text objects
+    ['o ih'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
+    ['x ih'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
+  },
+})
