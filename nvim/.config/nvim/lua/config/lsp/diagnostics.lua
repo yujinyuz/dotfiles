85d2b51b68d2f6d985f8c4ce9590df2f0bec1c42
Eugene Oliveros <eevoliveros@gmail.com>
2021-11-28 22:08:53 +0800

    fix(nvim): prepare for v0.6

--- nvim/.config/nvim/lua/config/lsp/diagnostics.lua
+++ nvim/.config/nvim/lua/config/lsp/diagnostics.lua
@@ -1,26 +1,6 @@
--- My settings
--- vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
---   vim.lsp.diagnostic.on_publish_diagnostics, {
---     -- underline = false,
---     -- virtual_text = false,
---     -- signs = true,
---     -- update_in_insert = false,
---     -- virtual_text = {
---     --   prefix = "»",
---     --   spacing = 4,
---     -- },
---     -- virtual_text = true,
---     virtual_text = {spacing = 2, severity_limit = 'Error'},
---     signs = false,
---     update_in_insert = false,
---     underline = true,
---   }
--- )
 local M = {}
 
 M.setup = function()
-  -- Automatically update diagnostics
-  -- from folke/dot
   vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
     underline = true,
     update_in_insert = false,
@@ -28,10 +8,10 @@ M.setup = function()
     severity_sort = true,
   })
 
-  local signs = { Error = ' ', Warning = ' ', Hint = ' ', Information = ' ' }
+  local signs = { Error = ' ', Warn = ' ', Hint = ' ', Info = ' ' }
 
   for type, icon in pairs(signs) do
-    local hl = 'LspDiagnosticsSign' .. type
+    local hl = 'DiagnosticSign' .. type
     vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
   end
 end
