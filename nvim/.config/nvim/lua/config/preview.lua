f6d1e66e58556404026d4482d3e59a6ac88aac48
Eugene Oliveros <eevoliveros@gmail.com>
2021-12-15 21:09:41 +0800

    fix(nvim): pass in table to goto-preview

--- nvim/.config/nvim/lua/config/preview.lua
+++ nvim/.config/nvim/lua/config/preview.lua
@@ -1,8 +1,31 @@
 local gp = require('goto-preview')
 
-gp.setup()
+gp.setup({})
 
-vim.keymap.nnoremap({'gpd', function() gp.goto_preview_definition() end})
-vim.keymap.nnoremap({'gpi', function() gp.goto_preview_implementation() end})
-vim.keymap.nnoremap({'gpr', function() gp.goto_preview_references() end})
-vim.keymap.nnoremap({'gP', function() gp.close_all_win() end})
+vim.keymap.nnoremap({
+  'gpd',
+  function()
+    gp.goto_preview_definition()
+  end,
+})
+
+vim.keymap.nnoremap({
+  'gpi',
+  function()
+    gp.goto_preview_implementation()
+  end,
+})
+
+vim.keymap.nnoremap({
+  'gpr',
+  function()
+    gp.goto_preview_references()
+  end,
+})
+
+vim.keymap.nnoremap({
+  'gP',
+  function()
+    gp.close_all_win()
+  end,
+})
