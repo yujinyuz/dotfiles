6ad5f7c11cfe176f4979dea7d1c092676f46e2c4
Eugene Oliveros <eevoliveros@gmail.com>
2021-12-24 23:16:21 +0800

    fix(nvim): improve statusline

--- nvim/.config/nvim/lua/config/statusline.lua
+++ nvim/.config/nvim/lua/config/statusline.lua
@@ -25,8 +25,7 @@ lualine.setup({
   sections = {
     lualine_a = { 'mode' },
     lualine_b = { 'branch', 'diff', { 'diagnostics', sources = { 'nvim_diagnostic' } } },
-    -- lualine_c = { filename },
-    lualine_c = { 'filename' },
+    lualine_c = { { 'filename', file_status = true, path = 1 } },
     lualine_x = { { require('nvim-gps').get_location, cond = require('nvim-gps').is_available }, 'encoding', 'filetype' },
     lualine_y = { 'progress' },
     lualine_z = { 'location' },
@@ -34,8 +33,7 @@ lualine.setup({
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    -- lualine_c = { filename },
-    lualine_c = { 'filename' },
+    lualine_c = { { 'filename', file_status = true, path = 1 } },
     lualine_x = { 'location' },
     lualine_y = {},
     lualine_z = {},
