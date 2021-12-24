bc7bf22ff13e30bd319dca5772d74764f2bc1d77
Eugene Oliveros <eevoliveros@gmail.com>
2021-12-24 23:10:29 +0800

    perf(nvim): optimize fzf-lua config
    Finally it much looks better than before. I've removed the border,
    removed the file preview for files list and made the fzf.tags not scroll
    coz I don't need the data past tag name.

--- nvim/.config/nvim/lua/config/fzf.lua
+++ nvim/.config/nvim/lua/config/fzf.lua
@@ -2,6 +2,23 @@ local path = require('fzf-lua.path')
 local fzf = require('fzf-lua')
 
 fzf.setup({
+  winopts = {
+    height = 0.85, -- window height
+    width = 0.80, -- window width
+    row = 0.35, -- window row position (0=top, 1=bottom)
+    col = 0.50, -- window col position (0=left, 1=right)
+    hl = {
+      normal = 'TelescopePromptNormal',
+      border = 'TelescopeBorder',
+      cursor = 'Cursor', -- cursor highlight (grep/LSP matches)
+      cursorline = 'CursorLine', -- cursor line
+      search = 'Search', -- search matches (ctags)
+      -- title       = 'Normal',        -- preview border title (file/buffer)
+    },
+  },
+  fzf_opts = {
+    ['--border'] = 'none',
+  },
   fzf_colors = {
     ['fg'] = { 'fg', 'CursorLine' },
     ['bg'] = { 'bg', 'Normal' },
@@ -16,7 +33,6 @@ fzf.setup({
     ['spinner'] = { 'fg', 'Label' },
     ['header'] = { 'fg', 'Comment' },
     ['gutter'] = { 'bg', 'Normal' },
-    ['border'] = { 'bg', 'Normal' },
   },
   keymap = {
     builtin = {
@@ -24,46 +40,52 @@ fzf.setup({
       ['<C-u>'] = 'preview-page-up',
     },
   },
-  hl = {
-    normal = 'TelescopePromptNormal',
-    border = 'TelescopeBorder',
-  },
   files = {
     git_icons = false,
+    multiprocess = true,
   },
   git = {
     files = {
       cmd = 'git ls-files --exclude-standard --cached --others --deduplicate',
+      previewer = { _ctor = false },
       git_icons = false,
+      file_icons = false,
+      multiprocess = true,
     },
   },
   grep = {
     rg_opts = string.format('%s %s', fzf.config.globals.grep.rg_opts, '--hidden -g "!.git"'),
     git_icons = false,
+    multiprocess = true,
   },
+  file_icon_padding = '',
+  on_create = function()
+    vim.keymap.tnoremap({ '<Esc>', '<C-c>', buffer = true })
+  end,
 })
 
 vim.keymap.nnoremap({
   '<leader>]',
   function()
-    fzf.tags({ fzf_cli_args = '--nth=2..' })
+    fzf.tags({ fzf_cli_args = '--nth=2.. --no-hscroll' })
   end,
 })
 
 vim.keymap.nnoremap({
   '<leader>F',
   function()
-    fzf.live_grep({ fzf_cli_args = '--nth=2..' })
+    fzf.live_grep({ fzf_cli_args = '--nth=2..', exec_empty_query = true })
   end,
 })
 vim.keymap.nnoremap({
   '<leader>n',
   function()
-    if path.is_git_repo(vim.fn.getcwd(), true) then
-      fzf.git_files()
+    if path.is_git_repo(vim.loop.cwd(), true) then
+      fzf.git_files({ fzf_opts = { ['--ansi'] = false }, file_icons = false, git_icons = false })
       return
     end
-    fzf.files()
+    fzf.files({ fzf_opts = { ['--ansi'] = false }, file_icons = false, git_icons = false })
   end,
 })
 vim.keymap.nnoremap({ '<leader>bb', '<Cmd>FzfLua buffers<CR>' })
+vim.keymap.nnoremap({ '<leader>fw', '<Cmd>FzfLua grep_cword<CR>' })
