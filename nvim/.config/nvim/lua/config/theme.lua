febbc6b00ff04f7c30dce1016088aaff11f46b61
Eugene Oliveros <eevoliveros@gmail.com>
2021-12-24 23:17:12 +0800

    feat(nvim): use rose-pine as new colorscheme
    I'm kinda switching to rosepine now but might switch soon to
    kanagawa.nvim as it kinda has much better color palette. But they don't
    have a config for fish yet.

--- nvim/.config/nvim/lua/config/theme.lua
+++ nvim/.config/nvim/lua/config/theme.lua
@@ -1,6 +1,7 @@
-require('tokyonight').colorscheme()
+vim.cmd('colorscheme rose-pine')
+-- require('tokyonight').colorscheme()
 
 -- Make tab numbers visible
 -- Title          xxx ctermfg=225 gui=bold guifg=#7aa2f7
 -- vim.cmd [[hi clear Title]]
-vim.cmd [[ hi Title guifg=#c0caf5 gui=italic]]
+-- vim.cmd [[ hi Title guifg=#c0caf5 gui=italic]]
