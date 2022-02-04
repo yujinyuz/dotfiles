52cdc82061ad31f5de9d6e8f9159fe7bf9b6abc0
Eugene Oliveros <eevoliveros@gmail.com>
2022-02-04 19:36:54 +0800

    refactor(nvim): use vim philosophy for nvim-cmp triggers
    Ever since @tjdevries mentioned something about being a tab user in vim,
    I've always wondered what he meant by it. He did mention that he will
    explain it in the future.
    
    But reading from the comments, he recommends looking at
    `:h ins-completion` which I kinda did not understand at first.
    
    I mean I knew it was there but it just did not make sense.
    
    It was until I saw `nvim-cmp`'s documentation that they have an example
    on how to replace `omnifunc`.
    
    There, it suggests `i_<C-x><C-o>` to trigger lsp completion.
    
    It's working fine for me. I don't mind the extra keystrokes though and I
    feel like it's much better to trigger different sources with different
    keys.
    
    For example, what I currently have is
    
    i_<C-x><C-o> for trigerring LSP completion items
    i_<C-x><C-]> for triggering ripgrep completion items
    i_<C-x><C-s> for triggering snippet items
    
    There could be more but you get the idea.

--- nvim/.config/nvim/after/ftplugin/vue.lua
+++
@@ -1,9 +0,0 @@
-require('cmp').setup.buffer({
-  sources = {
-    { name = 'nvim_lsp' },
-    { name = 'rg', max_item_count = 5, keyword_length = 3},
-    { name = 'path', max_item_count = 5 },
-  },
-})
-
-
