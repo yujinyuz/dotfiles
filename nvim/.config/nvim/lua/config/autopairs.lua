280352c96bd22c13e9228e4ac681ba043de29bd9
Eugene Oliveros <eevoliveros@gmail.com>
2022-03-06 17:29:10 +0800

    fix(nvim): disable autopairs in markdown files

--- nvim/.config/nvim/lua/config/autopairs.lua
+++ nvim/.config/nvim/lua/config/autopairs.lua
@@ -1,5 +1,5 @@
 require('nvim-autopairs').setup {
-  disable_filetype = { 'TelescopePrompt', 'vim' },
+  disable_filetype = { 'TelescopePrompt', 'vim', 'markdown' },
   map_c_w = true,
   check_ts = true,
 }
