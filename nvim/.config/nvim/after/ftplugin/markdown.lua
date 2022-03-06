bcca8c883d2cc9bf554667a85bcc8df867f728ca
Eugene Oliveros <eevoliveros@gmail.com>
2022-03-06 17:21:46 +0800

    feat(nvim): add preview and textwidth for markdown
    - Toggle markdown preview via `<leader>pm`
    - Add textwidth to markdown files

--- nvim/.config/nvim/after/ftplugin/markdown.lua
+++ nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -1,6 +1,6 @@
 -- Doing this instead ot the lua version because `vim.opt_local` seems to be quite buggy as of now
 vim.cmd([[
-  setlocal spell conceallevel=3 concealcursor=n
+  setlocal spell conceallevel=3 concealcursor=n textwidth=100
 ]])
 -- vim.opt_local.spell = true
 -- vim.opt_local.conceallevel = 3
@@ -12,3 +12,5 @@ vim.keymap.set('i', ';t', '## <C-R>=strftime("%H:%M")<CR><CR><CR><CR>', { buffer
 
 -- Mnemonic for [i]dea
 vim.keymap.set('n', '<leader>i', 'Go<CR>## <C-R>=strftime("%H:%M")<CR><Esc>zzA<CR><CR>', { buffer = 0, silent = true })
+
+vim.keymap.set('n', '<leader>pm', '<Cmd>MarkdownPreviewToggle<CR>', { buffer = 0 })
