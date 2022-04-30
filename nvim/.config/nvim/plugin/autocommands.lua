1253ef9027d66f8415a8489b3915a5a1c8ede03d
Eugene Oliveros <eevoliveros@gmail.com>
2022-04-30 16:24:17 +0800

    feat(nvim): port autocommands.rc.vim to lua
    Converted most of the things inside autocommands.rc.vim into lua since
    the nvim 0.7 finally released the `nvim_create_autocmd`.
    
    I'm not entirely sure about the cons of just having a single augroup but
    I'll just leave it there for awhile

---
+++ nvim/.config/nvim/plugin/autocommands.lua
@@ -0,0 +1,103 @@
+local ide_group = vim.api.nvim_create_augroup('IDECallbacks', { clear = true })
+
+-- Return to last edit position
+vim.api.nvim_create_autocmd('BufReadPost', {
+  pattern = { '*' },
+  group = ide_group,
+  callback = function()
+    local ft = vim.opt_local.filetype:get()
+    -- don't apply to git messages
+    if ft:match('commit') or ft:match('rebase') then
+      return
+    end
+    -- get position of last saved edit
+    local line, col = unpack(vim.api.nvim_buf_get_mark(0, '"'))
+    -- if in range, go there
+    if (line > 1) and (line <= vim.api.nvim_buf_line_count(0)) then
+      vim.api.nvim_win_set_cursor(0, { line, col })
+    end
+  end,
+  desc = 'Return to Last Edit Position',
+})
+
+-- Highight on yank
+vim.api.nvim_create_autocmd('TextYankPost', {
+  pattern = { '*' },
+  group = ide_group,
+  callback = function()
+    vim.highlight.on_yank {}
+  end,
+  desc = 'Highight on Yank',
+})
+
+-- Strip Trailing Whitespace
+vim.api.nvim_create_autocmd('BufWritePre', {
+  pattern = { '*' },
+  group = ide_group,
+  callback = function()
+    local ft = vim.opt_local.filetype:get()
+    if ft:match('commit') or ft:match('rebase') then
+      return
+    end
+
+    local cursor_pos = vim.api.nvim_win_get_cursor(0)
+    vim.cmd([[%s/\s\+$//e]])
+    vim.api.nvim_win_set_cursor(0, cursor_pos)
+  end,
+  desc = 'Strip Trailing Whitespace',
+})
+
+vim.api.nvim_create_autocmd('TermOpen', {
+  pattern = { '*' },
+  group = ide_group,
+  callback = function()
+    vim.opt_local.signcolumn = 'no'
+  end,
+  desc = 'Start Terminal Insert',
+})
+
+vim.api.nvim_create_autocmd({
+  'BufEnter',
+  'FocusGained',
+  'InsertLeave',
+  'WinEnter',
+}, {
+  pattern = { '*' },
+  group = ide_group,
+  callback = function()
+    if vim.opt_local.number and vim.fn.mode ~= 'i' then
+      vim.opt_local.relativenumber = true
+    end
+  end,
+  desc = 'Enable relativenumber',
+})
+
+vim.api.nvim_create_autocmd({
+  'BufLeave',
+  'FocusLost',
+  'InsertEnter',
+  'WinLeave',
+}, {
+  pattern = { '*' },
+  group = ide_group,
+  callback = function()
+    if vim.opt_local.number then
+      vim.opt_local.relativenumber = false
+    end
+  end,
+  desc = 'Disable relative number',
+})
+
+vim.api.nvim_create_autocmd('VimResized', {
+  pattern = { '*' },
+  group = ide_group,
+  command = 'wincmd =',
+  desc = 'Automatically Resize Windows Equally',
+})
+
+vim.api.nvim_create_autocmd('BufWritePre', {
+  pattern = { '*' },
+  group = ide_group,
+  command = "let &bex = substitute(expand('%:p:h'), '/', ':', 'g') . strftime('%F.%H:%M')",
+  desc = 'Rename backup files',
+})
