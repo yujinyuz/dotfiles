9c2ae5350629782f261884a7f9fc5bd4bc28c40c
Eugene Oliveros <yujin@jinyuz.dev>
2022-05-04 09:59:20 +0800

    feat(nvim): switch to neo-tree

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -132,7 +132,12 @@ local plugins = function(use)
   }
 
   use {
-    'kyazdani42/nvim-tree.lua',
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v2.x',
+    requires = {
+      'nvim-lua/plenary.nvim',
+      'MunifTanjim/nui.nvim',
+    },
     config = function()
       require('config.tree')
     end,
