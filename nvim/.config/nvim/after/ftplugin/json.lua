4217f403f29a485cd1cec2f33e2dfd4fc3dc2a37
Eugene Oliveros <yujin@jinyuz.dev>
2022-05-29 18:39:49 +0800

    fix(nvim): use vim.opt_local for ftplugin/*

--- nvim/.config/nvim/after/ftplugin/json.lua
+++ nvim/.config/nvim/after/ftplugin/json.lua
@@ -1,6 +1,10 @@
-vim.bo.shiftwidth = 4
-vim.bo.tabstop = 4
+vim.opt_local.shiftwidth = 4
+vim.opt_local.tabstop = 4
+
+
+
 
 vim.cmd([[ command! Format :%!jq .]])
 
-vim.keymap.set('n', '<leader>cf', '<Cmd>Format<CR>', { silent = true, buffer = 0 })
+vim.keymap.set('n', '<leader>cf', '<Cmd>Format<CR>', { silent = true, buffer = 0, desc = 'Format JSON' })
+
