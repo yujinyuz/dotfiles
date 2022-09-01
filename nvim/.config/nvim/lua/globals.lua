d3e470d523caaac119561272cc9b1fecf0cb0b27
Eugene Oliveros <yujin@jinyuz.dev>
2022-09-02 00:30:36 +0800

    fix(nvim): fix filetypes
    I think I was using https://github.com/nathom/filetype.nvim before but I think this is not necessary when 0.8 comes

--- nvim/.config/nvim/lua/globals.lua
+++ nvim/.config/nvim/lua/globals.lua
@@ -26,7 +26,7 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.g.loaded_netrwSettings = 1
 
-vim.g.did_load_filetypes = 1
+-- vim.g.did_load_filetypes = 1
 
 -- @usage dump(vim.opt)
 _G.dump = function(...)
