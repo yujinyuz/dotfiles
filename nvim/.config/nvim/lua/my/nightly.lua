039a331b92b6ee1a581b56188d27885d8d660a4c
Eugene Oliveros <yujin@jinyuz.dev>
2022-10-16 18:38:15 +0800

    chore(nvim): remove outdated nightly config

--- nvim/.config/nvim/lua/my/nightly.lua
+++ nvim/.config/nvim/lua/my/nightly.lua
@@ -3,24 +3,3 @@ if not vim.version().prerelease then
   return
 end
 
-vim.opt.cmdheight = 0
-
--- Coz having `laststatus=3` makes other windows filename not get displayed
-vim.opt.winbar = vim.opt.winbar
-  + "%{winnr() > 9 ? ' ':''}"
-  + "%{winnr() == 1 ? ' ':''}"
-  + "%{winnr() == 2 ? ' ':''}"
-  + "%{winnr() == 3 ? ' ':''}"
-  + "%{winnr() == 4 ? ' ':''}"
-  + "%{winnr() == 5 ? ' ':''}"
-  + "%{winnr() == 6 ? ' ':''}"
-  + "%{winnr() == 7 ? ' ':''}"
-  + "%{winnr() == 8 ? ' ':''}"
-  + "%{winnr() == 9 ? ' ':''}"
-  + " %{expand('%') == '' ? '[No Name]' : pathshorten(expand('%:~:.'))} "
-  -- + '%='
-  -- + "%{&modified?' ':''}"
-  + "%{&modified?'[+] ':''}"
-  + "%{&readonly?' ':''}"
-  -- + "%{&paste?'  ':''}"
-  + "%{&spell?' ¶ ':''}"
