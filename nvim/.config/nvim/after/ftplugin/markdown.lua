4068e23f34a3c33c80b5187da6f7f150f95757d0
Eugene Oliveros <yujin@jinyuz.dev>
2022-11-05 09:38:30 +0800

    fix(nvim): set conceallevel to 0 for markdown

--- nvim/.config/nvim/after/ftplugin/markdown.lua
+++ nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -1,6 +1,7 @@
 vim.opt_local.spell = true
-vim.opt_local.conceallevel = 3
+vim.opt_local.conceallevel = 0
 vim.opt_local.concealcursor = 'n'
+-- vim.opt_local.textwidth = 100
 
 vim.keymap.set('i', ';H', '<Esc>yypv$r=', { buffer = 0 })
 vim.keymap.set('i', ';h', '<Esc>yypv$r-', { buffer = 0 })
