2b9b72a1e1ee0b13c63fe5432a104b411986eb41
Eugene Oliveros <yujin@jinyuz.dev>
2023-04-02 15:29:03 +0800

    feat(nvim): add new null-ls diagnostics and formatters

--- nvim/.config/nvim/lua/configs/null-ls.lua
+++ nvim/.config/nvim/lua/configs/null-ls.lua
@@ -5,23 +5,32 @@ if not has_nls then
 end
 
 local sources = {
+
+  nls.builtins.diagnostics.cspell.with { filetypes = { 'markdown', 'txt' } },
+  nls.builtins.code_actions.cspell.with { filetypes = { 'markdown', 'txt' } },
+
+  nls.builtins.diagnostics.dotenv_linter,
+  nls.builtins.diagnostics.fish,
+  nls.builtins.diagnostics.hadolint,
+  -- nls.builtins.diagnostics.write_good,
+
+  -- Formatting
   nls.builtins.formatting.prettierd,
   nls.builtins.formatting.djhtml.with { extra_args = { '--tabwidth', '2' } },
-  -- nls.builtins.formatting.prettier,
   nls.builtins.formatting.eslint_d,
   nls.builtins.formatting.black,
-  nls.builtins.formatting.codespell.with { filetypes = { 'markdown', 'txt' } },
+  -- nls.builtins.formatting.codespell.with { filetypes = { 'markdown', 'txt' } },
   nls.builtins.formatting.reorder_python_imports,
   nls.builtins.formatting.taplo,
-  nls.builtins.formatting.fish_indent,
-  -- nls.builtins.formatting.isort,
   nls.builtins.formatting.stylua,
-  nls.builtins.diagnostics.codespell.with { filetypes = { 'markdown', 'txt' } },
   nls.builtins.formatting.fish_indent,
   nls.builtins.formatting.fixjson.with { filetypes = { 'jsonc' } },
-  -- nls.builtins.diagnostics.write_good,
-  nls.builtins.diagnostics.flake8,
+  nls.builtins.formatting.ruff,
+
+  -- Diagnostics
+  -- nls.builtins.diagnostics.codespell.with { filetypes = { 'markdown', 'txt' } },
   nls.builtins.diagnostics.eslint.with { command = 'eslint_d' },
+  nls.builtins.diagnostics.ruff,
 }
 
 local lsp_format = require('my.lsp-format')
