17117d7670d64a8ab7a27840c75777eb1ddfde50
Eugene Oliveros <yujin@jinyuz.dev>
2023-04-02 15:25:00 +0800

    fix(nvim): indent json files with 4 spaces

--- nvim/.config/nvim/after/ftplugin/json.lua
+++ nvim/.config/nvim/after/ftplugin/json.lua
@@ -1,6 +1,6 @@
 vim.opt_local.shiftwidth = 4
 vim.opt_local.tabstop = 4
 
-vim.api.nvim_create_user_command('Format', ':%!jq .', {})
+vim.api.nvim_create_user_command('Format', ':%!jq . --indent 4', {})
 
 vim.keymap.set('n', '<leader>cf', '<Cmd>Format<CR>', { silent = true, buffer = 0, desc = 'Format JSON' })
