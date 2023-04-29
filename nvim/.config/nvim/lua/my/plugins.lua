86dbd977d4c565a01d164796c689ce78ef66cb92
Eugene Oliveros <yujin@jinyuz.dev>
2023-04-29 16:35:35 +0800

    chore(nvim): add custom color for highlights

--- nvim/.config/nvim/lua/my/plugins.lua
+++ nvim/.config/nvim/lua/my/plugins.lua
@@ -394,7 +394,7 @@ local plugins = {
     end,
     config = function()
       require('kanagawa').setup {
-        compile = true,
+        compile = false,
         globalStatus = true,
         colors = {
           theme = {
@@ -406,9 +406,30 @@ local plugins = {
           },
         },
         overrides = function(colors)
+          local theme = colors.theme
           return {
             NormalFloat = { bg = 'none' },
             FloatBorder = { bg = 'none' },
+            FloatTitle = { bg = 'none' },
+
+            LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+            MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+
+            GitpadFloatBorder = { fg = theme.ui.float.fg_border },
+            GitpadFloatTitle = { fg = theme.ui.special },
+
+            Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },
+            PmenuSel = { fg = 'none', bg = theme.ui.bg_p2 },
+            PmenuSbar = { bg = theme.ui.bg_m1 },
+            PmenuThumb = { bg = theme.ui.bg_p2 },
+
+            TelescopeTitle = { fg = theme.ui.special, bold = true },
+            TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+            TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+            TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+            TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+            TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+            TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
           }
         end,
       }
