8b33c9d097f1c0116c698621d09b349ac524cffc
Eugene Oliveros <yujin@jinyuz.dev>
2023-05-03 09:11:40 +0800

    chore(nvim): remove deprecated nerdicons

--- nvim/.config/nvim/lua/my/options.lua
+++ nvim/.config/nvim/lua/my/options.lua
@@ -141,22 +141,21 @@ vim.opt.laststatus = 3
 
 -- Coz having `laststatus=3` makes other windows filename not get displayed
 vim.opt.winbar = vim.opt.winbar
-  + "%{winnr() > 9 ? ' ':''}"
-  + "%{winnr() == 1 ? ' ':''}"
-  + "%{winnr() == 2 ? ' ':''}"
-  + "%{winnr() == 3 ? ' ':''}"
-  + "%{winnr() == 4 ? ' ':''}"
-  + "%{winnr() == 5 ? ' ':''}"
-  + "%{winnr() == 6 ? ' ':''}"
-  + "%{winnr() == 7 ? ' ':''}"
-  + "%{winnr() == 8 ? ' ':''}"
-  + "%{winnr() == 9 ? ' ':''}"
+  + "%{winnr() > 9 ? '󰏁 ':''}"
+  + "%{winnr() == 1 ? '󰎥 ':''}"
+  + "%{winnr() == 2 ? '󰎨 ':''}"
+  + "%{winnr() == 3 ? '󰎫 ':''}"
+  + "%{winnr() == 4 ? '󰎲 ':''}"
+  + "%{winnr() == 5 ? '󰎯 ':''}"
+  + "%{winnr() == 6 ? '󰎴 ':''}"
+  + "%{winnr() == 7 ? '󰎷 ':''}"
+  + "%{winnr() == 8 ? '󰎺 ':''}"
+  + "%{winnr() == 9 ? '󰎽 ':''}"
   + " %{expand('%') == '' ? '[No Name]' : pathshorten(expand('%:~:.'))} "
   -- + '%='
   -- + "%{&modified?' ':''}"
   + "%{&modified?'[+] ':''}"
   + "%{&readonly?' ':''}"
-  -- + "%{&paste?'  ':''}"
   + "%{&spell?' ¶ ':''}"
 
 -- Use <Space> as the leader key
