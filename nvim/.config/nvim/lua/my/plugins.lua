01dd6d0a1e8d04800469daaf0215c5d9c99c0332
Eugene Oliveros <yujin@jinyuz.dev>
2023-05-08 09:36:07 +0800

    chore(nvim): use custom colors

--- nvim/.config/nvim/lua/my/plugins.lua
+++ nvim/.config/nvim/lua/my/plugins.lua
@@ -446,24 +446,17 @@ local plugins = {
             FloatBorder = { bg = 'none' },
             FloatTitle = { bg = 'none' },
 
-            LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-            MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+            LazyNormal = { bg = theme.ui.bg_p1, fg = theme.ui.fg_dim },
+            MasonNormal = { bg = theme.ui.bg_p1, fg = theme.ui.fg_dim },
+            WhichKeyFloat = { bg = theme.ui.bg_p1, fg = theme.ui.fg_dim },
 
-            GitpadFloatBorder = { fg = theme.ui.float.fg_border },
-            GitpadFloatTitle = { fg = theme.ui.special },
+            GitpadFloatBorder = { bg = 'NONE', fg = theme.ui.float.fg_border },
+            GitpadFloatTitle = { bg = 'none', fg = theme.ui.special },
 
             Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },
             PmenuSel = { fg = 'none', bg = theme.ui.bg_p2 },
             PmenuSbar = { bg = theme.ui.bg_m1 },
             PmenuThumb = { bg = theme.ui.bg_p2 },
-
-            TelescopeTitle = { fg = theme.ui.special, bold = true },
-            TelescopePromptNormal = { bg = theme.ui.bg_p1 },
-            TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
-            TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
-            TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
-            TelescopePreviewNormal = { bg = theme.ui.bg_dim },
-            TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
           }
         end,
       }
