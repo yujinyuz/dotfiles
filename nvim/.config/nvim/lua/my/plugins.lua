32a1d05228a5eff6df710b109c7597e91a3043b1
Eugene Oliveros <yujin@jinyuz.dev>
2023-05-14 22:23:07 +0800

    feat(nvim): replace indent-blankline with mini.indentscope
    It's not that mini.indentscope is better. It's just that it's minimal
    and I don't use indent-blankline much anyways

--- nvim/.config/nvim/lua/my/plugins.lua
+++ nvim/.config/nvim/lua/my/plugins.lua
@@ -542,31 +542,17 @@ local plugins = {
     },
   },
   {
-    'lukas-reineke/indent-blankline.nvim',
+    'echasnovski/mini.indentscope',
+    version = false,
     init = function()
-      vim.cmd([[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]])
-      vim.cmd([[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]])
-      vim.cmd([[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]])
-      vim.cmd([[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]])
-      vim.cmd([[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]])
-      vim.cmd([[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]])
+      vim.g.miniindentscope_disable = true
     end,
-    cmd = 'IndentBlanklineToggle',
-    opts = {
-      enabled = false,
-      show_current_context = true,
-      show_current_context_start = true,
-      show_end_of_line = true,
-      space_char_blankline = ' ',
-      char_highlight_list = {
-        'IndentBlanklineIndent1',
-        'IndentBlanklineIndent2',
-        'IndentBlanklineIndent3',
-        'IndentBlanklineIndent4',
-        'IndentBlanklineIndent5',
-        'IndentBlanklineIndent6',
-      },
-    },
+    config = function()
+      require('mini.indentscope').setup {
+        symbol = '│',
+        options = { try_as_border = true },
+      }
+    end,
   },
   {
     'j-hui/fidget.nvim',
