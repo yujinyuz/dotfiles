749dd558bfaa1a87ed50d83a77a8eabd6fee908a
Eugene Oliveros <yujin@jinyuz.dev>
2024-03-16 15:37:41 +0800

    feat(nvim): add mini hipatterns
    This is quite useful especiall highlighting NOTES and TODOs within the
    codebase

--- nvim/.config/nvim/lua/my/plugins.lua
+++ nvim/.config/nvim/lua/my/plugins.lua
@@ -652,6 +652,24 @@ local plugins = {
     end,
   },
   {
+    'echasnovski/mini.hipatterns',
+    version = false,
+    config = function()
+      local hipatterns = require('mini.hipatterns')
+      hipatterns.setup {
+        highlighters = {
+          -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
+          fixme = { pattern = '%f[%w]()FIXME()%f[%W]', group = 'MiniHipatternsFixme' },
+          hack = { pattern = '%f[%w]()HACK()%f[%W]', group = 'MiniHipatternsHack' },
+          todo = { pattern = '%f[%w]()TODO()%f[%W]', group = 'MiniHipatternsTodo' },
+          note = { pattern = '%f[%w]()NOTE()%f[%W]', group = 'MiniHipatternsNote' },
+          -- Highlight hex color strings (`#rrggbb`) using that color
+          hex_color = hipatterns.gen_highlighter.hex_color(),
+        },
+      }
+    end,
+  },
+  {
     'j-hui/fidget.nvim',
     event = { 'BufRead' },
     opts = {},
