28c095ed2bb9e940e4f67b9683c1d1928a944a73
Eugene Oliveros <yujin@jinyuz.dev>
2024-03-17 21:08:56 +0800

    refactor(nvim): convert existing keymaps to use nvim apis
    I just learned about what expr is in vim and decided to convert some of
    the keymaps I made to either use expr or the available vim apiis

--- nvim/.config/nvim/after/ftplugin/markdown.lua
+++ nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -8,12 +8,27 @@ vim.opt_local.formatprg = 'safe-par rTbqR B=.,\\?_A_a_0 Q=_s\\>\\| -w' .. vim.op
 vim.keymap.set('i', ';H', '<Esc>yypv$r=', { buffer = 0 })
 vim.keymap.set('i', ';h', '<Esc>yypv$r-', { buffer = 0 })
 
-vim.keymap.set('i', ';d', '## <C-R>=strftime("%A, %B %d, %Y")<CR><CR><CR>', { buffer = 0 })
-vim.keymap.set('i', ';D', '### <C-R>=strftime("%Y-%m-%d")<CR><CR><CR>', { buffer = 0 })
-vim.keymap.set('i', ';t', '### <C-R>=strftime("%H:%M")<CR><CR><CR>', { buffer = 0 })
+vim.keymap.set('i', ';d', function()
+  return vim.fn.strftime('## %A, %B %d, %Y\n\n\n')
+end, { buffer = 0, expr = true })
+
+vim.keymap.set('i', ';D', function()
+  return vim.fn.strftime('### %Y-%m-%d\n\n\n')
+end, { buffer = 0, expr = true })
+
+vim.keymap.set('i', ';t', function()
+  return vim.fn.strftime('### %H:%M\n\n\n')
+end, { buffer = 0, expr = true })
 
 -- Mnemonic for [i]dea
-vim.keymap.set('n', '<leader>i', 'Go<CR>## <C-R>=strftime("%H:%M")<CR><Esc>zzA<CR><CR>', { buffer = 0, silent = true })
+
+vim.keymap.set('n', '<leader>i', function()
+  local n_lines = vim.api.nvim_buf_line_count(0)
+  local lines = { '', '## ' .. vim.fn.strftime('%H:%M'), '', '' }
+  vim.api.nvim_buf_set_lines(0, -1, -1, true, lines)
+  vim.api.nvim_win_set_cursor(0, { #lines + n_lines, 0 })
+  vim.cmd.startinsert()
+end, { buffer = 0 })
 
 vim.keymap.set('n', '<leader>pm', '<Cmd>MarkdownPreviewToggle<CR>', { buffer = 0 })
 
