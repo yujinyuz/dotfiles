f2883b1f84fda6631980bb03a314605b82ef3813
Eugene Oliveros <yujin@jinyuz.dev>
2024-03-24 02:48:35 +0800

    chore(nvim): remove nvim-hlslens plugin
    I don't think it's useful anymore since there's already searchcount
    functionality in below the statusline when performing a search

--- nvim/.config/nvim/lua/my/plugins.lua
+++ nvim/.config/nvim/lua/my/plugins.lua
@@ -306,17 +306,6 @@ local plugins = {
     end,
   },
   {
-    'kevinhwang91/nvim-hlslens',
-    keys = {
-      { 'n', [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]] },
-      { 'N', [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]] },
-      { '*', [[*<Cmd>lua require('hlslens').start()<CR>]] },
-      { '#', [[#<Cmd>lua require('hlslens').start()<CR>]] },
-      { 'g*', [[g*<Cmd>lua require('hlslens').start()<CR>]] },
-      { 'g#', [[g#<Cmd>lua require('hlslens').start()<CR>]] },
-    },
-  },
-  {
     'stevearc/aerial.nvim',
     opts = {},
     cmd = { 'AerialToggle' },
