45065662f301aa2338ba8774984c6064a2af9da8
Eugene Oliveros <yujin@jinyuz.dev>
2024-03-24 13:51:24 +0800

    perf(nvim): lazy load copilotchat

--- nvim/.config/nvim/lua/my/plugins.lua
+++ nvim/.config/nvim/lua/my/plugins.lua
@@ -847,28 +847,39 @@ local plugins = {
       { 'zbirenbaum/copilot.lua' }, -- or github/copilot.vim
       { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
     },
-    config = function()
-      require('CopilotChat').setup { debug = true }
-
-      vim.keymap.set({ 'n', 'x' }, '<leader>cch', function()
-        local actions = require('CopilotChat.actions')
-        require('CopilotChat.integrations.fzflua').pick(actions.help_actions())
-      end)
-
-      vim.keymap.set({ 'n', 'x' }, '<leader>ccp', function()
-        local actions = require('CopilotChat.actions')
-        require('CopilotChat.integrations.fzflua').pick(actions.prompt_actions())
-      end)
-
-      vim.keymap.set('n', '<leader>ccq', function()
-        local input = vim.fn.input('Quick Chat: ')
-        if input ~= '' then
-          require('CopilotChat').ask(input, { selection = require('CopilotChat.select').buffer })
-        end
-      end)
-
-      vim.keymap.set('n', '<leader>cgc', '<Cmd>CopilotChatCommitStaged<CR>', { silent = true })
-    end,
+    opts = { debug = true },
+    keys = {
+      {
+        '<leader>cch',
+        function()
+          local actions = require('CopilotChat.actions')
+          require('CopilotChat.integrations.fzflua').pick(actions.help_actions())
+        end,
+        mode = { 'n', 'x' },
+      },
+      {
+        '<leader>ccq',
+        function()
+          local input = vim.fn.input('Quick Chat: ')
+          if input ~= '' then
+            require('CopilotChat').ask(input, { selection = require('CopilotChat.select').buffer })
+          end
+        end,
+        mode = 'n',
+      },
+      {
+        '<leader>ccp',
+        function()
+          local actions = require('CopilotChat.actions')
+          require('CopilotChat.integrations.fzflua').pick(actions.prompt_actions())
+        end,
+      },
+      {
+        '<leader>cgc',
+        '<Cmd>CopilotChatCommitStaged<CR>',
+        mode = 'n',
+      },
+    },
   },
   --endblock: Miscellaneous
 }
