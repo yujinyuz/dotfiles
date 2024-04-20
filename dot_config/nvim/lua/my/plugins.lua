2601b521e5d8841db75cba1e35935fa21de9bb1c
Eugene Oliveros <yujin@jinyuz.dev>
2024-04-20 15:45:36 +0800

    fix(nvim): replace deprecated commands
    neovim has deprecated some vim.api functions.
    Better to replace them now

--- dot_config/nvim/lua/my/plugins.lua
+++ dot_config/nvim/lua/my/plugins.lua
@@ -713,9 +713,9 @@ local plugins = {
       saturation = 0,
       window_ignore_function = function(winid)
         local bufid = vim.api.nvim_win_get_buf(winid)
-        local buftype = vim.api.nvim_buf_get_option(bufid, 'buftype')
+        local buftype = vim.api.nvim_get_option_value('buftype', { buf = bufid })
         local floating = vim.api.nvim_win_get_config(winid).relative ~= ''
-        local diff_mode = vim.api.nvim_win_get_option(winid, 'diff')
+        local diff_mode = vim.api.nvim_get_option_value('diff', { win = winid })
 
         -- Do not tint `terminal` or floating windows, tint everything else
         return buftype == 'terminal' or floating or diff_mode
