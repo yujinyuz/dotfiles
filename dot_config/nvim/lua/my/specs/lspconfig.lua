174f07b1eab40805db44be4ccd581afd7caf6e8d
Eugene Oliveros <yujin@jinyuz.dev>
2024-05-11 16:08:49 +0800

    chore(nvim): update lspconfig spec
    Notes
    
    - Disabled semanticTokensProvider
    - Added crn and crr keymaps. neovim nightly 0.10 added it and then
      removed it. So I'll just add it here for consistency and the desc
    - Removed mason tools installer as I think it's not needed anymore.
      I can install just via `:Mason` with the added ensure function
    - Changed debounce_text_changes to 500. Though not sure about the
      implication of this.
    - Removed workspace lsp keymaps as I don't seem to use it. Never know
      what its for
    - Let mason-lspconfig do the server setup via handlers. This ensures
      that the binary is available before even opening a file (in case it
      isn't installed yet.)

--- dot_config/nvim/lua/my/specs/lspconfig.lua
+++ dot_config/nvim/lua/my/specs/lspconfig.lua
@@ -1,179 +1,222 @@
-local has_lspconfig, lspconfig = pcall(require, 'lspconfig')
-
-if not has_lspconfig then
-  return
-end
-
-local utils = require('my.utils')
-
--- Set up completion using nvim_cmp with LSP source
-local common_capabilities = require('cmp_nvim_lsp').default_capabilities()
-common_capabilities.textDocument.foldingRange = {
-  dynamicRegistration = false,
-  lineFoldingOnly = true,
-}
-
-local common_on_init = function(client, _)
-  -- Disabled due to https://github.com/neovim/neovim/issues/23164
-  -- There has been a problem where there is a delay and then it changes the
-  -- highlighting.
-  client.server_capabilities.semanticTokensProvider = false
-end
-
-local common_on_attach_handler = function(client, bufnr)
-  local opts = {
-    buffer = bufnr,
-    silent = true,
-  }
-
-  -- +lsp
-  vim.keymap.set('n', '<leader>cla', vim.lsp.buf.add_workspace_folder, opts)
-  vim.keymap.set('n', '<leader>clr', vim.lsp.buf.remove_workspace_folder, opts)
-
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-  vim.keymap.set('n', 'gI', vim.lsp.buf.implementation, opts)
-  vim.keymap.set('i', '<C-s>', vim.lsp.buf.signature_help, opts)
-
-  if client.server_capabilities.definitionProvider ~= false then
-    vim.opt_local.tagfunc = 'v:lua.vim.lsp.tagfunc'
+local lsp_config = function()
+  -- Setup neoconfig before lspconfig
+  local has_neoconf, neoconf = pcall(require, 'neoconf')
+  if has_neoconf then
+    neoconf.setup {}
   end
 
-  if client.server_capabilities.documentSymbolProvider then
-    local has_navic, navic = pcall(require, 'nvim-navic')
-    if has_navic then
-      navic.attach(client, bufnr)
+  -- Set up completion using nvim_cmp with LSP source
+  local common_capabilities = require('cmp_nvim_lsp').default_capabilities()
+  common_capabilities.textDocument.foldingRange = {
+    dynamicRegistration = false,
+    lineFoldingOnly = true,
+  }
+
+  local common_on_init = function(client, _)
+    -- Disabled due to https://github.com/neovim/neovim/issues/23164
+    -- I've been experiencing this annoying issue where there is a slight delay of applying
+    -- highlights when opening certain files:
+    --  * lua suddenly changes highlights for global variables
+    --  * python (pyright and basedpyright) changes highlights and sometimes italicizes import
+    --    statements.
+    --  I do not want this. Just let treesitter perform the highlighting for now.
+    client.server_capabilities.semanticTokensProvider = false
+  end
+
+  local common_on_attach_handler = function(client, bufnr)
+    -- +lsp
+    vim.keymap.set('n', 'crn', vim.lsp.buf.rename, { buffer = bufnr, desc = '[c]ode [r]efactor [n]ame' })
+    vim.keymap.set('n', 'crr', vim.lsp.buf.code_action, { buffer = bufnr, desc = '[c]ode [r]efactor [r]efactor' })
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, { buffer = bufnr, desc = '[g]oto [r]eferences' })
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = bufnr, desc = '[g]oto [d]efinition' })
+    vim.keymap.set('n', 'gI', vim.lsp.buf.implementation, { buffer = bufnr, desc = '[g]oto [I]mplementation' })
+    vim.keymap.set('i', '<C-s>', vim.lsp.buf.signature_help, { buffer = bufnr, desc = 'code [s]ignature help' })
+
+    if client.server_capabilities.definitionProvider ~= false then
+      vim.opt_local.tagfunc = 'v:lua.vim.lsp.tagfunc'
+    end
+
+    if client.server_capabilities.documentSymbolProvider then
+      local has_navic, navic = pcall(require, 'nvim-navic')
+      if has_navic then
+        navic.attach(client, bufnr)
+      end
     end
   end
-end
 
--- Server config
-local servers = {
-  pyright = {
-    enabled = false,
-    settings = {
-      pyright = {
-        disableOrganizeImports = true,
-      },
-      python = {
-        analysis = {
-          -- Ignore all files for analysis to exclusively use Ruff for linting
-          ignore = { '*' },
+  -- Server config
+  local servers = {
+    pyright = {
+      enabled = false,
+      settings = {
+        pyright = {
+          disableOrganizeImports = true,
+        },
+        python = {
+          analysis = {
+            -- Ignore all files for analysis to exclusively use Ruff for linting
+            ignore = { '*' },
+          },
         },
       },
     },
-  },
-  basedpyright = {
-    settings = {
-      basedpyright = {
-        disableOrganizeImports = true,
-        -- https://github.com/DetachHead/basedpyright/issues/203
-        typeCheckingMode = 'off',
-      },
-    },
-  },
-  jedi_language_server = {
-    enabled = false,
-    init_options = {
-      completion = {
-        disableSnippets = true,
-      },
-    },
-  },
-  ruff_lsp = {
-    on_attach = function(client, bufnr)
-      common_on_attach_handler(client, bufnr)
-      client.server_capabilities.disableHoverProvider = false
-
-      -- Create ruff commands
-      vim.api.nvim_create_user_command('RuffAutoFix', function()
-        vim.lsp.buf.execute_command {
-          command = 'ruff.applyAutofix',
-          arguments = {
-            { uri = vim.uri_from_bufnr(0) },
-          },
-        }
-      end, { desc = 'Ruff: Fix all auto-fixable problems' })
-
-      vim.api.nvim_create_user_command('RuffOrganizeImports', function()
-        vim.lsp.buf.execute_command {
-          command = 'ruff.applyOrganizeImports',
-          arguments = {
-            { uri = vim.uri_from_bufnr(0) },
-          },
-        }
-      end, { desc = 'Ruff: Format imports' })
-    end,
-  },
-  gopls = {},
-  html = {},
-  cssls = {},
-  emmet_ls = {},
-  tailwindcss = {},
-  jsonls = {},
-  tsserver = {},
-  vuels = {},
-  lua_ls = {
-    Lua = {
-      workspace = { checkThirdParty = false },
-      telemetry = { enable = false },
-      completion = {
-        callSnippet = 'Disable',
-      },
-      -- https://github.com/LuaLS/lua-language-server/wiki/Formatter#json
-      format = {
-        enable = true,
-        defaultConfig = {
-          indent_style = 'space',
-          indent_size = '2',
+    basedpyright = {
+      enabled = true,
+      settings = {
+        basedpyright = {
+          disableOrganizeImports = true,
+          -- https://github.com/DetachHead/basedpyright/issues/203
+          typeCheckingMode = 'off',
         },
       },
     },
-  },
-  taplo = {},
-}
+    jedi_language_server = {
+      enabled = false,
+      init_options = {
+        completion = {
+          disableSnippets = true,
+        },
+      },
+    },
+    ruff_lsp = {
+      on_attach = function(client, bufnr)
+        common_on_attach_handler(client, bufnr)
+        client.server_capabilities.disableHoverProvider = false
 
-local options = {
-  enabled = true,
-  on_init = common_on_init,
-  on_attach = common_on_attach_handler,
-  capabilities = common_capabilities,
-  flags = {
-    debounce_text_changes = 500, -- 150 seems to be the default by idk what this implies
-  },
-}
+        -- Create ruff commands
+        vim.api.nvim_create_user_command('RuffAutoFix', function()
+          vim.lsp.buf.execute_command {
+            command = 'ruff.applyAutofix',
+            arguments = {
+              { uri = vim.uri_from_bufnr(0) },
+            },
+          }
+        end, { desc = 'Ruff: Fix all auto-fixable problems' })
 
-local has_mason, mason = pcall(require, 'mason')
+        vim.api.nvim_create_user_command('RuffOrganizeImports', function()
+          vim.lsp.buf.execute_command {
+            command = 'ruff.applyOrganizeImports',
+            arguments = {
+              { uri = vim.uri_from_bufnr(0) },
+            },
+          }
+        end, { desc = 'Ruff: Format imports' })
+      end,
+    },
+    gopls = {},
+    html = {},
+    cssls = {},
+    emmet_ls = {},
+    tailwindcss = {},
+    jsonls = {},
+    tsserver = {},
+    vuels = {},
+    lua_ls = {
+      Lua = {
+        workspace = { checkThirdParty = false },
+        telemetry = { enable = false },
+        completion = {
+          callSnippet = 'Disable',
+        },
+        codelens = {
+          enable = true,
+        },
+      },
+    },
+    taplo = {},
+  }
 
-if has_mason then
-  mason.setup {}
-end
+  local common_options = {
+    enabled = true,
+    on_init = common_on_init,
+    on_attach = common_on_attach_handler,
+    capabilities = common_capabilities,
+    flags = {
+      debounce_text_changes = 500, -- 150 seems to be the default by idk what this implies
+    },
+  }
 
-local has_mason_lspconfig, mason_lspconfig = pcall(require, 'mason-lspconfig')
+  local mason_lspconfig = require('mason-lspconfig')
+  local setup_server_handler = function(server_name)
+    if servers[server_name] == nil then
+      return
+    end
 
-if has_mason_lspconfig then
+    local server_opts = vim.tbl_deep_extend('force', common_options, servers[server_name] or {})
+
+    if server_opts.enabled ~= false then
+      require('lspconfig')[server_name].setup(server_opts)
+    end
+  end
+
   mason_lspconfig.setup {
     ensure_installed = vim.tbl_keys(servers),
+    handlers = { setup_server_handler },
   }
 end
 
--- Setup neodev before lspconfig
-local has_neodev, neodev = pcall(require, 'neodev')
+return {
+  {
+    'neovim/nvim-lspconfig',
+    event = { 'BufReadPre', 'BufNewFile' },
+    config = lsp_config,
+    dependencies = {
+      { 'folke/neoconf.nvim', cmd = 'Neoconf', config = false },
+      { 'folke/neodev.nvim', opts = {} },
+      { 'onsails/lspkind-nvim' },
+      {
+        'SmiteshP/nvim-navic',
+        keys = {
+          {
+            '<C-s>',
+            function()
+              print(require('nvim-navic').get_location())
+            end,
+            desc = 'Show current location',
+          },
+        },
+      },
+      'williamboman/mason.nvim',
+      'williamboman/mason-lspconfig.nvim',
+    },
+  },
+  {
+    'williamboman/mason.nvim',
+    cmd = 'Mason',
+    build = ':MasonUpdate',
+    opts = {
+      ensure_installed = {
+        'black',
+        'codespell',
+        'cspell',
+        'djlint',
+        'eslint_d',
+        'fixjson',
+        'hadolint',
+        'prettierd',
+        'stylua',
+        'taplo',
+        'write-good',
+      },
+    },
+    config = function(_, opts)
+      require('mason').setup(opts)
+      local mr = require('mason-registry')
 
-if has_neodev then
-  neodev.setup {}
-end
+      local function ensure_installed()
+        for _, tool in ipairs(opts.ensure_installed) do
+          local p = mr.get_package(tool)
+          if not p:is_installed() then
+            p:install()
+          end
+        end
+      end
 
--- Setup neoconfig before lspconfig
-local has_neoconf, neoconf = pcall(require, 'neoconf')
-
-if has_neoconf then
-  neoconf.setup {}
-end
-
-for server, custom_cfg in pairs(servers) do
-  local opts = vim.tbl_deep_extend('force', options, custom_cfg or {})
-  if opts.enabled then
-    lspconfig[server].setup(opts)
-  end
-end
+      if mr.refresh() then
+        mr.refresh(ensure_installed)
+      else
+        ensure_installed()
+      end
+    end,
+  },
+}
