c04619c761972ee6fa0de7f4227784a4140ca9d9
Eugene Oliveros <yujin@jinyuz.dev>
2024-05-18 22:40:30 +0800

    feat(nvim): add vim-dadbod and vim-dadbod-ui
    This plugin looks promising and has the potential to replace TablePlus
    for basic things I need to perform.

--- dot_config/nvim/lua/my/plugins.lua
+++ dot_config/nvim/lua/my/plugins.lua
@@ -988,6 +988,31 @@ local plugins = {
     opts = {},
     dev = true,
   },
+  {
+    'kristijanhusak/vim-dadbod-ui',
+    dependencies = {
+      { 'tpope/vim-dadbod', lazy = true },
+      { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
+    },
+    cmd = {
+      'DBUI',
+      'DBUIToggle',
+      'DBUIAddConnection',
+      'DBUIFindBuffer',
+    },
+    init = function()
+      -- Your DBUI configuration
+      vim.g.db_ui_use_nerd_fonts = 1
+    end,
+    keys = {
+      {
+        '<leader>D',
+        function()
+          vim.cmd('DBUIToggle')
+        end,
+      },
+    },
+  },
   --endblock: Miscellaneous
 }
 
