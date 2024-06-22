01e834f84fec3dc3a97e674f388f9d9bb0f456dd
Eugene Oliveros <yujin@jinyuz.dev>
2024-06-22 17:53:59 +0800

    refactor(nvim): use more lua code for statusline

--- dot_config/nvim/plugin/simple-statusline.lua
+++ dot_config/nvim/plugin/simple-statusline.lua
@@ -2,46 +2,80 @@ _G.SimpleStatusline = {}
 
 local H = {}
 
-H.fileprefix = function()
+H.is_truncated = function(trunc_width)
+  local cur_width = vim.o.laststatus == 3 and vim.o.columns or vim.api.nvim_win_get_width(0)
+  return cur_width < (trunc_width or -1)
+end
+
+H.fileprefix = function(args)
   local basename = vim.fn.fnamemodify(vim.fn.expand('%:h'), ':p:~:.')
   if basename == '' or basename == '.' then
     return ''
   else
-    return basename:gsub('/$', '') .. '/'
+    basename = basename:gsub('/$', '') .. '/'
+
+    if H.is_truncated(args.trunc_width) then
+      return vim.fn.pathshorten(basename)
+    end
+
+    return basename
   end
 end
 
-_G.SimpleStatusline.default = function()
+H.git_branch = function(args)
+  if H.is_truncated(args.trunc_width) then
+    return ''
+  end
+  return vim.b.gitsigns_head and string.format('  %s ', vim.b.gitsigns_head) or ''
+end
+
+H.file_info = function(args)
+  if H.is_truncated(args.trunc_width) then
+    return ''
+  end
+  local file_size = vim.b.bufsize_human
+
+  if not file_size then
+    return '%y '
+  end
+
+  return string.format('%s %s ', '%y', file_size)
+end
+
+H.loc_info = function(args)
+  if H.is_truncated(args.trunc_width) then
+    return 'ℓ:%l 𝚌:%2v'
+  end
+  -- return 'ℓ:%l/%L 𝚌:%2v/%-2{virtcol("$")-1} %p%%'
+  return 'ℓ:%l/%L 𝚌:%2v/%-2{virtcol("$")-1}'
+end
+
+_G.SimpleStatusline.render = function()
   return table.concat {
     -- Show mode
-    '[%{mode()}] ',
+    '%#StatusLineCommonInfo#',
+    string.format('[%s]', vim.api.nvim_get_mode().mode),
     -- Show git branch (if available)
-    vim.b.gitsigns_head and string.format(' %s ', vim.b.gitsigns_head) or '',
-    -- Show filepath where filepath is dimmed and filename is bold
+    H.git_branch { trunc_width = 80 },
+    '%*',
     '%<',
     '%#StatuslineFilePrefix#',
-    H.fileprefix(),
+    string.format(' %s', H.fileprefix { trunc_width = 120 }),
     '%#StatuslineFilename#',
     '%t',
     -- Reset highlight
     '%* ',
     -- Show modified flag and readonly flag
-    '%m ',
-    '%r ',
+    '%m%r%h%w',
     -- Start right aligned items
     '%=%*',
-    -- Show filetype
-    '%y ',
-    -- Show filesize
-    vim.b.bufsize_human and string.format('%s ', vim.b.bufsize_human) or '',
+    -- Show filetype and file size
+    H.file_info { trunc_width = 80 },
     -- Show current line and total lines
-    'ℓ:%l/%L ',
-    -- Show current column and total columns
-    '𝚌:%2v/%-2{virtcol("$")-1} ',
-    -- Show percentage
-    '%p%%',
+    '%#StatusLineLocInfo# ',
+    H.loc_info { trunc_width = 80 },
   }
 end
 
 -- Set statusline
-vim.opt.statusline = '%!v:lua.SimpleStatusline.default()'
+vim.opt.statusline = '%!v:lua.SimpleStatusline.render()'
