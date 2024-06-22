764cdb8119df59c4ff55311ec44c92aa9564b2d3
Eugene Oliveros <yujin@jinyuz.dev>
2024-06-22 17:49:00 +0800

    feat(nvim): add fugitive to close_with_q

--- dot_config/nvim/after/plugin/autocommands.rc.lua
+++ dot_config/nvim/after/plugin/autocommands.rc.lua
@@ -185,6 +185,7 @@ vim.api.nvim_create_autocmd('FileType', {
     'neotest-summary',
     'neotest-output-panel',
     'grug-far',
+    'fugitive',
   },
   callback = function(event)
     vim.bo[event.buf].buflisted = false
