4eed2d3380e3349889f038e9bca9a2a4aa5dd8d6
Eugene Oliveros <yujin@jinyuz.dev>
2024-07-06 15:12:53 +0800

    chore(nvim): cleanup markdown config

--- dot_config/nvim/after/ftplugin/markdown.lua
+++ dot_config/nvim/after/ftplugin/markdown.lua
@@ -3,8 +3,8 @@ vim.opt_local.conceallevel = 0
 vim.opt_local.concealcursor = 'n'
 vim.opt_local.shiftwidth = 2
 vim.opt_local.textwidth = 100
-vim.opt_local.formatprg = 'safe-par rTbqR B=.,\\?_A_a_0 Q=_s\\>\\| -w' .. vim.opt_local.textwidth:get()
-vim.opt_local.colorcolumn = { vim.opt_local.textwidth:get() }
+vim.opt_local.formatprg = 'safe-par rTbqR B=.,\\?_A_a_0 Q=_s\\>\\| -w' .. vim.bo.textwidth
+vim.opt_local.colorcolumn = { vim.bo.textwidth }
 
 vim.keymap.set('i', ';H', '<Esc>yypv$r=', { buffer = 0 })
 vim.keymap.set('i', ';h', '<Esc>yypv$r-', { buffer = 0 })
@@ -21,17 +21,4 @@ vim.keymap.set('i', ';t', function()
   return vim.fn.strftime('### %H:%M\n\n\n')
 end, { buffer = 0, expr = true })
 
--- Mnemonic for [i]dea
-
-vim.keymap.set('n', '<leader>i', function()
-  local n_lines = vim.api.nvim_buf_line_count(0)
-  local lines = { '', '## ' .. vim.fn.strftime('%H:%M'), '', '' }
-  vim.api.nvim_buf_set_lines(0, -1, -1, true, lines)
-  vim.api.nvim_win_set_cursor(0, { #lines + n_lines, 0 })
-  vim.cmd.startinsert()
-end, { buffer = 0 })
-
 vim.keymap.set('n', '<leader>pm', '<Cmd>MarkdownPreviewToggle<CR>', { buffer = 0 })
-
--- Fix textwidth
--- %!fold -w 60
