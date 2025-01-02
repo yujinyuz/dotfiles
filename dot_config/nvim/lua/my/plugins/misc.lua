bb4550432e2130ba6d5d06c2857ed412be7b0d52
Eugene Oliveros <yujin@jinyuz.dev>
2025-01-02 15:11:12 +0800

    feat(nvim): add kulala

--- dot_config/nvim/lua/my/plugins/misc.lua
+++ dot_config/nvim/lua/my/plugins/misc.lua
@@ -21,4 +21,54 @@ return {
       { '<leader>fml', '<cmd>CellularAutomaton make_it_rain<CR>', desc = 'Make it rain' },
     },
   },
+  {
+    'mistweaverco/kulala.nvim',
+    ft = 'http',
+    opts = function()
+      return {
+        default_view = 'headers_body',
+        winbar = true,
+        contenttypes = {
+          ['text/plain'] = {
+            formatter = function(body)
+              return body
+            end,
+          },
+          ['application/json'] = {
+            ft = 'json',
+            formatter = { 'jq', '.' },
+            pathresolver = require('kulala.parser.jsonpath').parse,
+          },
+          ['application/vnd.api+json'] = {
+            ft = 'json',
+            formatter = { 'jq', '.' },
+            pathresolver = require('kulala.parser.jsonpath').parse,
+          },
+        },
+      }
+    end,
+    keys = {
+      {
+        '<CR>',
+        function()
+          require('kulala').run()
+        end,
+        ft = 'http',
+      },
+      {
+        '<leader>r',
+        function()
+          require('kulala').run()
+        end,
+        ft = 'http',
+      },
+      {
+        '<leader>v',
+        function()
+          require('kulala').toggle_view()
+        end,
+        ft = 'http',
+      },
+    },
+  },
 }
